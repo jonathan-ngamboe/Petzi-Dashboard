4b3f38c45b33d01a607e40967d793da252b9df3d
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2023-11-08 18:08:00 +0100

    Set up the PetziApplication main controller with the entry point method for registering tickets via POST.

--- petzi/src/main/java/ch/hearc/petzi/PetziApplication.java
+++ petzi/src/main/java/ch/hearc/petzi/PetziApplication.java
@@ -1,13 +1,41 @@
 package ch.hearc.petzi;
 
+import ch.hearc.petzi.model.Ticket;
+import ch.hearc.petzi.repository.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @SpringBootApplication
+@RestController
 public class PetziApplication {
+
+	@Autowired
+	private TicketRepository ticketRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetziApplication.class, args);
 	}
 
+	@PostMapping("/saveTicket")
+	public ResponseEntity<String> saveTicket(@RequestBody Ticket ticket, HttpServletRequest request) {
+		String headerValue = request.getHeader("Header");
+		try {
+			// Enregistre le ticket dans la base de données
+			ticketRepository.save(ticket);
+			return ResponseEntity.ok()
+					.header("Header", headerValue)
+					.body("Ticket enregistré");
+		} catch (Exception e) {
+			// Gestion de l'erreur d'enregistrement
+			return ResponseEntity.internalServerError()
+					.body("Erreur lors de l'enregistrement du ticket : " + e.getMessage());
+		}
+	}
 }
