74b7e75df7d9f4e4de2dab4dbb75a12286c14e0a
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2023-11-08 17:04:40 +0100

    Initial commit

---
+++ petzi/src/main/java/ch/hearc/petzi/PetziApplication.java
@@ -0,0 +1,13 @@
+package ch.hearc.petzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetziApplication.class, args);
+	}
+
+}
