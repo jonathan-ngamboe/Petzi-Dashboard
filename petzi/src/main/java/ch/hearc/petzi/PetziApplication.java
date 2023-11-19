7392b01c29acb0cc70c383097d411eb5bb9b96ed
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2023-11-19 09:00:06 +0100

    Implémenté la logique de vérification de la signature HMAC pour les webhooks - Ajouté une méthode pour valider la signature HMAC en utilisant une clé secrète. - Inclus la vérification du timestamp pour empêcher la réutilisation des anciens messages.

--- petzi/src/main/java/ch/hearc/petzi/PetziApplication.java
+++ petzi/src/main/java/ch/hearc/petzi/PetziApplication.java
@@ -5,10 +5,18 @@ import ch.hearc.petzi.repository.JsonStorageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import jakarta.servlet.http.HttpServletRequest;
+
+import java.security.InvalidKeyException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 
 @SpringBootApplication
 @RestController
@@ -16,25 +24,77 @@ public class PetziApplication {
 
 	@Autowired
 	private JsonStorageRepository jsonStorageRepository;
+	private static final String SECRET_KEY = "clé_secrète_pour_HMAC";
+	private static final String petziDefaultVersion = "2";
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetziApplication.class, args);
 	}
 
-	@PostMapping("/store/{key}")
-	public ResponseEntity<String> saveJson(@PathVariable String key, @RequestBody String json, HttpServletRequest request) {
-		String headerValue = request.getHeader("Header");
+
+	private boolean isSignatureValid(String payload, String receivedSignatureHeader) {
 		try {
+			String[] parts = receivedSignatureHeader.split(",");
+			long timestamp = Long.parseLong(parts[0].split("=")[1]);
+			String receivedSignature = parts[1].split("=")[1];
+
+			// Vérifie que le timestamp est dans une fenêtre de temps acceptable
+			long currentTime = System.currentTimeMillis() / 1000;
+			if (Math.abs(currentTime - timestamp) > 30) {
+				return false;
+			}
+
+			String signedPayload = timestamp + "." + payload;
+			byte[] expectedSignature = calculateHMAC(signedPayload, SECRET_KEY);
+
+			// Conversion de la signature hexadécimale reçue en tableau d'octets
+			byte[] receivedSignatureBytes = javax.xml.bind.DatatypeConverter.parseHexBinary(receivedSignature);
+
+			// Compare en utilisant une fonction de comparaison en temps constant
+			return MessageDigest.isEqual(expectedSignature, receivedSignatureBytes);
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	private byte[] calculateHMAC(String data, String key) throws NoSuchAlgorithmException, InvalidKeyException {
+		SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+		Mac mac = Mac.getInstance("HmacSHA256");
+		mac.init(secretKey);
+		return mac.doFinal(data.getBytes(StandardCharsets.UTF_8));
+	}
+
+	@PostMapping("/store")
+	public ResponseEntity<String> saveJson(@PathVariable String key, @RequestBody String json, HttpServletRequest request) {
+		String petziSignature = request.getHeader("Petzi-Signature");
+		String petziVersion = request.getHeader("Petzi-Version");
+
+		// Vérifie la version
+		if (!petziDefaultVersion.equals(petziVersion)) {
+			return ResponseEntity.ok("Version non prise en charge.");
+		}
+
+		try {
+			// Vérifie la signature
+			if (!isSignatureValid(json, petziSignature)) {
+				// Enregistre l'erreur dans un log mais retourne une réponse 200 OK
+				return ResponseEntity.ok("Signature invalide, mais la requête a été reçue.");
+				//return ResponseEntity.status(HttpStatus.FORBIDDEN)
+				//		.body("Signature invalide.");
+			}
+
 			JsonStorage storage = new JsonStorage();
-			storage.setKey(key);
 			storage.setValue(json);
 			jsonStorageRepository.save(storage);
 			return ResponseEntity.ok()
-					.header("Header", headerValue)
+					.header("Petzi-Signature", petziSignature)
 					.body("JSON enregistré avec la clé : " + key);
 		} catch (Exception e) {
-			return ResponseEntity.internalServerError()
-					.body("Erreur lors de l'enregistrement du JSON : " + e.getMessage());
+			// Enregistre l'erreur dans un log mais retourne une réponse 200 OK
+			return ResponseEntity.ok("Erreur lors de l'enregistrement du JSON : " + e.getMessage());
+			//return ResponseEntity.internalServerError()
+			//		.body("Erreur lors de l'enregistrement du JSON : " + e.getMessage());
 		}
 	}
 
