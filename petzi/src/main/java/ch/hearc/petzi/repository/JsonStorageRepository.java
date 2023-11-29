e2abbd40154a25c9b452f4e01730216eaca736b4
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2023-11-29 20:50:44 +0100

    Changement du type de base de l'id de String en Long

--- petzi/src/main/java/ch/hearc/petzi/repository/JsonStorageRepository.java
+++ petzi/src/main/java/ch/hearc/petzi/repository/JsonStorageRepository.java
@@ -3,6 +3,6 @@ package ch.hearc.petzi.repository;
 import ch.hearc.petzi.model.JsonStorage;
 import org.springframework.data.repository.CrudRepository;
 
-public interface JsonStorageRepository extends CrudRepository<JsonStorage, String> {
+public interface JsonStorageRepository extends CrudRepository<JsonStorage, Long> {
     // Pas besoin d'ajouter d'autres méthodes
 }
