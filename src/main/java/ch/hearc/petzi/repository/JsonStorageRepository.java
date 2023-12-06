da0534b70c0ad2237016e51a2f0a144ee40b9581
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2023-12-06 15:28:05 +0100

    Corrections mineures

--- src/main/java/ch/hearc/petzi/repository/JsonStorageRepository.java
+++
@@ -1,8 +0,0 @@
-package ch.hearc.petzi.repository;
-
-import ch.hearc.petzi.model.JsonStorage;
-import org.springframework.data.repository.CrudRepository;
-
-public interface JsonStorageRepository extends CrudRepository<JsonStorage, Long> {
-    // Pas besoin d'ajouter d'autres méthodes
-}
