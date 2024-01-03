e8378df208dc569e3ca203a8baf481a0bd9bc7b2
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-01-04 00:57:05 +0100

    Ajout d'une annocation EntityScan pour que Spring détecte le JsonRecord déplacé vers un module commun

--- PetziHook/src/main/java/ch/hearc/heg/petziHook/PetziHookApplication.java
+++ PetziHook/src/main/java/ch/hearc/heg/petziHook/PetziHookApplication.java
@@ -2,10 +2,12 @@ package ch.hearc.heg.petziHook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-public class PetziHook {
+@EntityScan(basePackages = {"ch.hearc.heg.persistence"})
+public class PetziHookApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(PetziHook.class, args);
+		SpringApplication.run(PetziHookApplication.class, args);
 	}
 }
