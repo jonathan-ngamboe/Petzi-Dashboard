9501b2691fc02cf17371da06d33f3d41a2f85057
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-01-04 00:54:39 +0100

    Configuration du Main pour fonctionner avec Springboot

--- RTBI/src/main/java/ch/hearc/heg/rtbi/Main.java
+++
@@ -1,7 +0,0 @@
-package ch.hearc.heg.rtbi;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ RTBI/src/main/java/ch/hearc/heg/rtbi/RtbiApplication.java
@@ -0,0 +1,12 @@
+package ch.hearc.heg.rtbi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class RtbiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RtbiApplication.class, args);
+    }
+}
