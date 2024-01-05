a9f4d41512f87aafa9abcadce4315d83ada70a1f
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-01-05 13:30:41 +0100

    Ajout d'un déclencheur au démarrage de l'app pour peupler le cache avec la DB

--- RTBI/src/main/java/ch/hearc/heg/rtbi/RtbiApplication.java
+++ RTBI/src/main/java/ch/hearc/heg/rtbi/RtbiApplication.java
@@ -1,12 +1,31 @@
 package ch.hearc.heg.rtbi;
 
+import ch.hearc.heg.rtbi.clients.PetziHookClient;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import ch.hearc.heg.rtbi.services.StatisticsService;
+
+/**
+ * Classe principale de l'application.
+ */
 @SpringBootApplication
-
 public class RtbiApplication {
     public static void main(String[] args) {
         SpringApplication.run(RtbiApplication.class, args);
+    }
+
+    /**
+     * Déclenche l'envoi des données JSON déjà enregistrées dans la DB au RTBI pour l'enregistrement en cache.
+     *
+     * @param statisticsService le service de statistiques.
+     * @param petziHookClient le client REST pour l'application PetziHook.
+     * @return un CommandLineRunner qui sera exécuté au démarrage de l'application.
+     */
+    @Bean
+    CommandLineRunner triggerDataSend(PetziHookClient petziHookClient, StatisticsService statisticsService) {
+        return args -> petziHookClient.triggerDataSend(statisticsService);
     }
 }
